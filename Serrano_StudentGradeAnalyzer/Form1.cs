f1b602a2d8268fc1caf7eec961267cfbd759cfb3
denver <denver@MSI>
2026-04-13 10:07:04 +0800

    Add project files.

---
+++ Serrano_StudentGradeAnalyzer/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serrano_StudentGradeAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public struct Student
+        {
+            public string StudentNumber;
+            public string StudentName;
+            public double Prelim;
+            public double Midterm;
+            public double Final;
+            public double Average;
+            public string Remarks;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private double ComputeAverage(double prelim, double midterm, double final)
+        {
+            return (prelim + midterm + final) / 3.0;
+        }
+        public static string GetRemarks(double average)
+        {
+            return average >= 75 ? "Passed" : "Failed";
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("StudentNumber", "Student Number");
+            dataGridView1.Columns.Add("StudentName", "Student Name");
+            dataGridView1.Columns.Add("Prelim", "Prelim Grade");
+            dataGridView1.Columns.Add("Midterm", "Midterm Grade");
+            dataGridView1.Columns.Add("Final", "Final Grade");
+            dataGridView1.Columns.Add("Average", "Average");
+            dataGridView1.Columns.Add("Remarks", "Remarks");
+        }
+
+        
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Student s = new Student();
+
+                
+                s.StudentNumber = textBox1.Text;  
+                s.StudentName = textBox2.Text;   
+                s.Prelim = double.Parse(textBox3.Text);
+                s.Midterm = double.Parse(textBox4.Text); 
+                s.Final = double.Parse(textBox5.Text); 
+
+                
+                s.Average = ComputeAverage(s.Prelim, s.Midterm, s.Final);
+                s.Remarks = GetRemarks(s.Average);
+
+               
+                dataGridView1.Rows.Add(
+                    s.StudentNumber,
+                    s.StudentName,
+                    s.Prelim,
+                    s.Midterm,
+                    s.Final,
+                    s.Average.ToString("F2"),
+                    s.Remarks
+                );
+            }
+            catch
+            {
+                MessageBox.Show("Please enter valid numeric grades!", "Input Error");
+            }
+        
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            dataGridView1.Rows.Clear();
+
+            textBox1.Focus();
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Serrano_StudentGradeAnalyzer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Serrano_StudentGradeAnalyzer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Serrano_StudentGradeAnalyzer")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09938b17-82e5-4ff4-8b5e-d44965d6a87a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
